439d29df898f4a16dbbf07786d1b9abf5aff038a
Luigi Scognamiglio <54768922+lui-esse@users.noreply.github.com>
2019-09-14 13:45:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// Create a random number
+var rollPlayer1 = Math.random() * 6;
+rollPlayer1 = Math.floor(rollPlayer1) + 1;
+// Select a random image
+var randomImage = "dice" + rollPlayer1 + ".png";
+// Select image source
+var randomImageSource = "images/" + randomImage;
+// Change attributes w Dom
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+// 2nd dice
+var rollPlayer2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + rollPlayer2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//
+if (rollPlayer1 > rollPlayer2) {
+  document.querySelector(".score").innerHTML = "🎲 Player 1 Wins 🎲";
+} else if (rollPlayer2 > rollPlayer1) {
+  document.querySelector(".score").innerHTML = "🎲 Player 2 Wins 🎲";
+} else {
+  document.querySelector(".score").innerHTML = "🎲 Draw 🎲";
+}
